c8341fafa6ee3c7ea5139d429a4f38816b27f2e9
Formador <Formador@192.168.3.18>
2017-06-21 20:08:22 +0200

    Commit -> explicación inicialización con valores e instancia

--- src/com/cice/fundamentos/Arreglos.java
+++ src/com/cice/fundamentos/Arreglos.java
@@ -19,12 +19,29 @@ public class Arreglos {
         //declaracion de arrays
         int[] numeros = {1, 2};
         int otrosNumeros[];
+        int[] otros;
         
         int[][] bidi = { {1, 2}, {1, 2} };
         
         int[][][] tresd = { { {1, 2}, {1, 2, 3}, {0, 1, 2} }, { {1, 2}, {1} , {0, 0, 1, 2, 5, 7} }, { {0, 0, 1, 2, 5, 7} } };
         
+        int nombres[][];
         
+        int[] edades[] = new int[2][2];
+        String[] tipos[][] = new String[2][2][2];
+        
+        tipos[0][0][1] = "25";
+        
+        //[3][3][6]
+        //tresd[0][0][2] = 0;
+        
+        for (String[][] c : tipos) {
+            for(String[] d : c) {
+                for(String v : d){
+                    System.out.println(v);
+                }
+            }
+        }
     }
     
 }
